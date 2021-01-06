1752ba6b45685a0c4dc3be76d231f0e5675bcdfb
khushi2018 <75610698+khushi2018@users.noreply.github.com>
2021-01-06 22:15:37 +0530

    4 program
    interchanging two numbers

---
+++ swapping.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h> 
+void main()
+{
+    int a,b, temp;
+    clrscr(); 
+    printf("Enter two integersn");
+    scanf("%d%d", &a, &b);
+    printf("Before Swapping\nFirst variable = %d\nSecond variable = %dn", a, b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After Swapping\nFirst variable = %d\nSecond variable = %dn", a, b);
+    getch();
+}
